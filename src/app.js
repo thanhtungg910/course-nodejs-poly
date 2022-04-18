6ac8cdf7f1ae8d05680a43ca633fe3a2d7b6955d
Thanh Tungg <trantungg910@gmail.com>
2022-04-18 13:51:09 +0700

    Fix: app

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
 import express from "express";
 import morgan from "morgan";
 import db from "./config/dbconfig";
-import { default as productList } from "./routes/products";
-import { default as categoryList } from "./routes/categories";
-import { default as userList } from "./routes/user";
+// import { default as productList } from "./routes/products";
+// import { default as categoryList } from "./routes/categories";
+// import { default as userList } from "./routes/user";
 const app = express();
 //MIDDLEWARE
 app.use(morgan());
 app.use(express.json());
 //ROUTING
-app.use("/api/products", productList);
-app.use("/api/categories", categoryList);
-app.use("/api/users", userList);
+// app.use("/api/products", productList);
+// app.use("/api/categories", categoryList);
+// app.use("/api/users", userList);
 
 app.get("/", (req, res) => {
-	res.send("<h1>HOME PAGE</h1>");
+     res.send("<h1>HOME PAGE</h1>");
 });
 
 //CONNECT DB
@@ -22,5 +22,5 @@ db.connect();
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-	console.log("Running port " + PORT);
+     console.log("Running port " + PORT);
 });
