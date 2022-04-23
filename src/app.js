4188507c4d71a78ab12353f2adf8220c5828eb01
Thanh Tungg <trantungg910@gmail.com>
2022-04-23 20:51:28 +0700

    Fix: app.js

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
 import express from "express";
 import morgan from "morgan";
 import db from "./config/dbconfig";
-// import { default as productList } from "./routes/products";
-// import { default as categoryList } from "./routes/categories";
-// import { default as userList } from "./routes/user";
+import { default as productList } from "./routes/products";
+import { default as categoryList } from "./routes/categories";
+import { default as userList } from "./routes/user";
 const app = express();
 //MIDDLEWARE
 app.use(morgan());
 app.use(express.json());
 //ROUTING
-// app.use("/api/products", productList);
-// app.use("/api/categories", categoryList);
-// app.use("/api/users", userList);
+app.use("/api/products", productList);
+app.use("/api/categories", categoryList);
+app.use("/api/users", userList);
 
 app.get("/", (req, res) => {
      res.send("<h1>HOME PAGE</h1>");
