4a4aefeeb37dcecf1b40a065e42707453b1128c9
Thanh Tungg <trantungg910@gmail.com>
2022-04-23 20:55:26 +0700

    >>

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import express from "express";
-import morgan from "morgan";
+// import morgan from "morgan";
 import db from "./config/dbconfig";
 import { default as productList } from "./routes/products";
 import { default as categoryList } from "./routes/categories";
 import { default as userList } from "./routes/user";
 const app = express();
 //MIDDLEWARE
-app.use(morgan());
+// app.use(morgan());
 app.use(express.json());
 //ROUTING
 app.use("/api/products", productList);
