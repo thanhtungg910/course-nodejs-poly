33e5952fb6a481da5cd8d57da164017c098f0ef2
Thanh Tungg <trantungg910@gmail.com>
2022-04-24 10:24:00 +0700

    Fix: products, cate

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,22 +1,22 @@
 import express from "express";
 import check from "../middleware/check";
 import {
-	getAll,
-	addProduct,
-	update,
-	getProduct,
-	deleteProduct,
-	findByPrice,
+     getAll,
+     addProduct,
+     update,
+     getProduct,
+     deleteProduct,
+     findByPrice,
 } from "../controllers/products";
 const router = express.Router();
 
-router.get("/", check, getAll);
+router.get("/", /* check, */ getAll);
 
 router.post("/", check, addProduct);
 
-router.get("/:slug", check, getProduct);
+router.get("/:slug", /* check, */ getProduct);
 
-router.get("/price_gte=:gte&price_lte=:lte", check, findByPrice);
+router.get("/price_gte=:gte&price_lte=:lte", /* check, */ findByPrice);
 
 router.patch("/:slug", check, update);
 
